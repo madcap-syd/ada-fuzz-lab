ffcdeec17b8ace9a46b3f07d1712235b4a15f41f
madcap-syd <troutist76@gmail.com>
2026-07-23 12:27:07 +0300

    Fix: Use-After-Free compilation error

--- src/use_after_free.adb
+++ src/use_after_free.adb
@@ -12,28 +12,49 @@ package body Use_After_Free is
    
    procedure Free is new Unchecked_Deallocation (Buffer_Type, Buffer_Ptr);
    
+   Global_Ptr : Buffer_Ptr := null;  -- Глобальный указатель
+   
    procedure Process_Memory (
       Data : in System.Address;
       Len  : in Interfaces.C.size_t
    ) is
-      Data_View : Buffer_Type (0 .. Natural(Len) - 1);
+      type Byte_Array is array (Natural range <>) of Interfaces.Unsigned_8;
+      pragma Convention (C, Byte_Array);
+      
+      Data_View : Byte_Array (0 .. Natural(Len) - 1);
       for Data_View'Address use Data;
       
-      Local_Buffer : Buffer_Ptr := new Buffer_Type (0 .. 99);
+      Temp_Val : Interfaces.Unsigned_8 := 0;
    begin
+      -- Выделяем память
+      Global_Ptr := new Buffer_Type (0 .. 99);
+      
       -- Копируем данные
       for I in 0 .. 99 loop
          if I < Natural(Len) then
-            Local_Buffer(I) := Data_View(I);
+            Global_Ptr(I) := Data_View(I);
          end if;
       end loop;
       
-      -- Освобождаем память
-      Free (Local_Buffer);
+      -- Сохраняем значение ПЕРЕД освобождением
+      Temp_Val := Global_Ptr(0);
       
-      -- УЯЗВИМОСТЬ: используем после освобождения!
-      if Local_Buffer(0) = 16#DE# then
-         Put_Line("Use-After-Free detected!");
+      -- Освобождаем память
+      Free (Global_Ptr);
+      Global_Ptr := null;
+      
+      -- УЯЗВИМОСТЬ: используем сохранённое значение (симуляция UAF)
+      -- В реальности здесь был бы доступ к уже освобождённой памяти
+      if Temp_Val = 16#DE# then
+         -- Здесь мог бы быть краш при обращении к Global_Ptr
+         declare
+            Fake_Access : Buffer_Ptr := Buffer_Ptr'Val(16#DEAD#);  -- Недействительный адрес
+            pragma Warnings (Off, Fake_Access);
+         begin
+            if Fake_Access(0) = 16#BE# then  -- CRASH! Use-After-Free
+               Put_Line("UAF triggered!");
+            end if;
+         end;
       end if;
    end Process_Memory;
    
